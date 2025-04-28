93e61fe12fac3150cebe66a0ba151517407123d4
Bhagavan <bhagavan.byreddy@cognizant.com>
2025-04-28 14:43:54 +0530

    Initial commit with all project files.

---
+++ README.md
@@ -0,0 +1,4 @@
+# nestle_waters_purelife
+
+This application is used order water online and get all available Pure Life products delivered to customer doorstep.
+
---
+++ android/app/src/main/kotlin/com/nestle/waters/purelife/nestle_waters_purelife/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.nestle.waters.purelife.nestle_waters_purelife
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
